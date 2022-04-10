a159149cb1d7f4e5ffbfaf89e0b082a419c9d3e5
fengkangderenlei <85933692+fengkangderenlei@users.noreply.github.com>
2022-04-10 19:42:00 +0800

    Update sync_v2p.js

--- sync_v2p.js
+++ sync_v2p.js
@@ -3,7 +3,7 @@ by CenBoMin
 感谢作者！
 **/
 const $ = new Env('elecv2pCk')
-const v2purl = $.getval('http://192.168.1.172:8100')
+const v2purl = $.getval('192.168.1.172:8100')
 const v2ptoken = $.getval('d71549bb-b344-4941-9bc3-0914dc5457a1');
 $.v2pcookieupdate = $.getdata("v2pcookieupdate") ? $.getdata("v2pcookieupdate") === "true" : false;
 //++++++++++++++++++++++++++++++++++++++++
